f7c256e471f2cab2e5be54cd7d997f0dcfa41eea
haixuwang <719259931@qq.com>
2019-12-15 23:02:18 +0800

    新增记录表，照片表，增加记录表

--- src/main/java/io/renren/modules/sys/controller/SysTaskController.java
+++ src/main/java/io/renren/modules/sys/controller/SysTaskController.java
@@ -4,9 +4,9 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.renren.common.annotation.SysLog;
 import io.renren.common.exception.RRException;
 import io.renren.common.utils.R;
-import io.renren.modules.sys.entity.SysOrgEntity;
-import io.renren.modules.sys.entity.SysTaskEntity;
+import io.renren.modules.sys.entity.*;
 import io.renren.modules.sys.service.SysOrgService;
+import io.renren.modules.sys.service.SysRecordService;
 import io.renren.modules.sys.service.SysTaskService;
 import org.apache.commons.lang.StringUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * 组织结构管理
@@ -27,6 +28,9 @@ public class SysTaskController extends AbstractController {
 
 	@Autowired
 	private SysTaskService sysTaskService;
+
+	@Autowired
+	private SysRecordService sysRecordService;
 
 	/**
 	 * 所有组织结构列表
@@ -49,5 +53,34 @@ public class SysTaskController extends AbstractController {
 			return R.error(1,"获取录入信息出错!");
 		}
 	}
+
+	@PostMapping("saveTask")
+	public R saveTask(@RequestBody SysTask systask){
+		try {
+
+			//获取任务
+			SysTaskEntity sysTaskEntity = systask.getSysTaskEntity();
+			sysTaskEntity.setId(UUID.randomUUID().toString());
+
+			sysTaskService.saveTaskEntity(sysTaskEntity);
+			//获取照片
+			List<TaskPicEntity> picEntityList = systask.getPicList();
+			//新建记录表
+			SysRecordEntity sysRecordEntity = new SysRecordEntity();
+			sysRecordEntity.setId(UUID.randomUUID().toString());
+			sysRecordEntity.setPid(sysTaskEntity.getId());
+			sysRecordEntity.setCreuser(sysTaskEntity.getCreuserid());
+			sysRecordEntity.setUser(sysTaskEntity.getCreuserid());
+			sysRecordEntity.setForuser(sysTaskEntity.getAssigner());
+			sysRecordEntity.setStatus(0L);
+			sysRecordService.saveRecord(sysRecordEntity);
+
+			return R.ok();
+		}catch (Exception e){
+			e.printStackTrace();
+			return R.error(1,"保存任务出错！");
+		}
+	}
+
 
 }
